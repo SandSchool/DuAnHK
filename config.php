d74669cb21d24ed4a779b42233754fd94e7bfd4c
Sandschool <trantruong7826@gmail.com>
2025-05-31 12:35:30 +0700

    commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTITCNTT02/DuAn1/DuAnBruh/',
+    'baseURL' => 'http://localhost/PTITCNTT02/DuAn1/DuAnBruh/',
+    'assets' => '/PTITCNTT02/DuAn1/DuAnBruh/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'gaybangiay',
+        'username' => 'root',
+        'password' => ''
+    )
+);
